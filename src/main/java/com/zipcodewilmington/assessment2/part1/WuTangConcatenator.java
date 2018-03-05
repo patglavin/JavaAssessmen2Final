f3cfcc1a964cc9e763f620fda07db8d65c177b3d
Patrick Glavin <patjglavin@gmail.com>
2018-03-05 09:17:32 -0500

    asdf

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -3,5 +3,9 @@ package com.zipcodewilmington.assessment2.part1;
 public class WuTangConcatenator{
     public int input;
 
+    public WuTangConcatenator(int input){
+        this.input = input;
+    }
 
+//    public boolean isWu
 }
